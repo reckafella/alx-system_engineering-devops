41824f15575227caf3564e870f0ca6949e87ad10
reckafella <ethanmuthoniw6@gmail.com>
2023-10-19 17:51:31 +0300

    add mandatory task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Resolve the error: Too many open files on Nginx Server
+
+exec {'fix-too-many-open-files':
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 9999\"/" /etc/default/nginx'
+}
+
+exec {'restart-nginx':
+  command => 'sudo service nginx restart'
+  require => Exec['fix-too-many-open-files']
+}
