f4e3542476abca1bc960c3a51402af75da747a2f
reckafella <ethanmuthoniw6@gmail.com>
2023-10-28 16:05:54 +0300

    update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 '''
 Module containing a function that queries the Reddit API and prints the
 titles of the first 10 hot posts for a given subreddit
@@ -7,6 +7,7 @@ Print None if an invalid subreddit is given
 '''
 import requests
 
+
 def top_ten(subreddit):
     '''
     function queries the Reddit API and prints the titles of
@@ -14,7 +15,7 @@ def top_ten(subreddit):
     '''
     url = 'https://api.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
     headers = {'user-agent': 'test_app'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
