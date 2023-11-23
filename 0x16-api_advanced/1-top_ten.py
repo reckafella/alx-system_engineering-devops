89664e51bad28363c301876de1e3daff58a64d31
Reckafella <ethanmuthoni@gmail.com>
2023-11-23 16:09:02 +0300

    Code works fine. Intranet disagrees!!!

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ titles of the first 10 hot posts for a given subreddit
 
 Print None if an invalid subreddit is given
 '''
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
@@ -13,27 +13,25 @@ def top_ten(subreddit):
     function queries the Reddit API and prints the titles of
     the first 10 hot posts
     '''
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'user-agent': 'personal app'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-agent': 'Firefox Developer 118.0.2044.18'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code != 200:
         print(None)
+        return None
+
+    results = response.json()
 
     try:
-        data = response.json()
+        data = results.get('data').get('children')
 
-    except ValueError as error:
+        for index in data:
+            print(index.get('data').get('title'))
+
+    except Exception:
         print(None)
-
-    try:
-        data = data.get('data')
-        children = data.get('children')
-
-        for child in children[:10]:
-            title = child.get('data').get('title')
-            print(title)
-
-    except Exception as error:
-        print(None)
+        return None
